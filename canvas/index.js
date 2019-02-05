9ae3f1cf98079d5d3768c2b87fd15c14315b585f
Chris Gallarno <cgallarno200@gmail.com>
2019-02-05 13:55:58 -0500

    Again

--- canvas/index.js
+++ canvas/index.js
@@ -35,6 +35,29 @@ function init() {
     findxy('out', e)
   }, false);
 
+  // Set up touch events for mobile, etc
+  canvas.addEventListener("touchstart", function (e) {
+    mousePos = getTouchPos(canvas, e);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousedown", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
+  canvas.addEventListener("touchend", function (e) {
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
+  canvas.addEventListener("touchmove", function (e) {
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousemove", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
+
   db.collection("Strokes").doc('1').onSnapshot(function(doc) {
     var context = display.getContext('2d');
     context.clearRect(0, 0, w, h);
@@ -102,4 +125,14 @@ function findxy(res, e) {
       draw();
     }
   }
+}
+
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
 }
